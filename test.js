bb2990bc2a70e3d6638f3825e3b7bca0e5f455e8
Onkar_Gaikwad <onkargaikwad97@gmail.com>
2023-07-22 19:57:04 +0530

    hello all of you

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 
 var app = require('../index.js');describe('GET /', function() {
 
- it('respond with hey man', function(done) { //navigate to root and check the the response is "hello world"
+ it('respond with hello all of you', function(done) { //navigate to root and check the the response is "hello world"
 
  request(app).get('/').expect('hello all of you', done);
 
